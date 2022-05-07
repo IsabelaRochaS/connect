1d27e6123906296e841639c65c5eb1970111e103
isabelarochas <bela.rsantos@gmail.com>
2022-05-07 10:58:27 -0300

    Setting up our React app

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home" style={{ backgroundImage: "url(/Layout-Ampulhetas.png)" }}>
+      <div className="lander">
+        <h1>AtuarConnect</h1>
+        <p className="text-muted">WIP</p>
+      </div>
+    </div>
+  );
+}
